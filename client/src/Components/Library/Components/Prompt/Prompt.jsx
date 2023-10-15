10f99049ce51d1e83d2bb0c22c9cd95ca9bf3e79
Pankaj Vatwani <vatwanipankaj74@gmail.com>
2023-10-15 19:45:14 +0530

    Feedback

--- client/src/Components/Library/Components/Prompt/Prompt.jsx
+++ client/src/Components/Library/Components/Prompt/Prompt.jsx
@@ -206,7 +206,7 @@ export function Prompt({ prompt, deletePrompts }) {
                     </span>
                   </Button>
                 </div>
-                <p className="bg-[#303030] mt-2 text-[12px] p-3 h-5/7">
+                <p className="bg-[#303030] mt-2 text-[12px] p-3 h-5/7" style={{ whiteSpace: 'pre-wrap'}}>
                   {prompt?.prompt}
                 </p>
                 <button
